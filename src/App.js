96f8c5f6e74fa8109509ed8dd26ecb7c9bb7bb12
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-10-04 17:31:36 -0300

    style: centered text on deletion, bold text in home

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
     <>
       <Navbar></Navbar>
       <div className='w-3/4 mx-auto flex flex-col text-center'>
-        <h1 className='text-2xl font-bold'>Bienvenido a la Base de Datos de Libros en MongoDB</h1>
+        <h1 className='text-2xl font-bold m-5'>Bienvenido a la Base de Datos de Libros en MongoDB</h1>
         <img src={Mongo} alt="" className='w-1/3 mx-auto' />
         <p>Este proyecto fue hecho con React de Front-End y Tide (de Rust) de Back-End</p>
-        <p>Después de apretar una opción, espere unos segundos</p>
+        <p><b>Después de apretar una opción, espere unos segundos.</b></p>
       </div>
     </>
   );
