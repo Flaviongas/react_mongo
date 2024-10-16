704b1e8e4a114566a2c5fd485258d8fd86395728
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-10-16 11:55:45 -0300

    style: a couple of hover effects

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,10 +7,10 @@ export default function Navbar() {
         <div className="container-fluid min-h-24">
           <div className="mx-auto ">
             <ul className="nav navbar-nav flex flex-row justify-around pt-10">
-              <li><Link id="len1" className="hoverable" to="/insert">Insertar </Link></li>
-              <li><Link id="len2" className="hoverable" to="/search">Buscar </Link></li>
-              <li><Link id="len3" className="hoverable" to="/update">Modificar </Link></li>
-              <li><Link id="len4" className="hoverable" to="/delete">Eliminar </Link></li>
+              <li><Link id="nav" className="hoverable" to="/insert">Insertar </Link></li>
+              <li><Link id="nav" className="hoverable" to="/search">Buscar </Link></li>
+              <li><Link id="nav" className="hoverable" to="/update">Modificar </Link></li>
+              <li><Link id="nav" className="hoverable" to="/delete">Eliminar </Link></li>
             </ul>
           </div>
         </div>
